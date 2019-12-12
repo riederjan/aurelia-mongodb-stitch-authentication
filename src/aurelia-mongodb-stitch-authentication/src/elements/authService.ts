b55a664b6c19ef6ba8154f2fb1d4e3135d97fe19
janrieder <jan.rieder@hevs.ch>
2019-12-12 14:57:14 +0100

    Added AuthService Class
    Added AuthService Class and some content

---
+++ src/aurelia-mongodb-stitch-authentication/src/elements/authService.ts
@@ -0,0 +1,10 @@
+import { Stitch, RemoteMongoClient, UserPasswordAuthProviderClient, UserPasswordCredential } from 'mongodb-stitch-browser-sdk';
+import { Container } from 'aurelia-dependency-injection';
+import { inject } from 'aurelia-framework';
+
+export class AuthService  {
+	
+	constructor() {
+		
+	}
+}
