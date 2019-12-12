c9e9936933cd7cb2cf7fc45a0f40e1853c6c7baa
janrieder <jan.rieder@hevs.ch>
2019-12-12 17:06:05 +0100

    Added debug functions in the authService
    Added debug functions in the authService

--- src/aurelia-mongodb-stitch-authentication/src/elements/authService.ts
+++ src/aurelia-mongodb-stitch-authentication/src/elements/authService.ts
@@ -1,10 +1,24 @@
 import { Stitch, RemoteMongoClient, UserPasswordAuthProviderClient, UserPasswordCredential } from 'mongodb-stitch-browser-sdk';
 import { Container } from 'aurelia-dependency-injection';
-import { inject } from 'aurelia-framework';
 
-export class AuthService  {
-	
-	constructor() {
-		
+export class AuthService {
+	appid: string;
+	email: string;
+	password: string;
+
+	show(what) {
+		if (what == "appid") {
+			console.log(this.appid);
+		}
+		else if (what == "email") {
+			console.log(this.email);
+		}
+		else if (what == "password") {
+			console.log(this.password);
+		}
+	}
+
+	login() {
+		// ...
 	}
 }
