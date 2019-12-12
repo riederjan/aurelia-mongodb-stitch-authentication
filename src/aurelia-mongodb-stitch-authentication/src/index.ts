714f286a0a3053ed1b8a00fcbb8c5a7b54653d02
janrieder <jan.rieder@hevs.ch>
2019-12-12 17:05:20 +0100

    Removed hello-world sample
    Removed hello-world sample

--- src/aurelia-mongodb-stitch-authentication/src/index.ts
+++ src/aurelia-mongodb-stitch-authentication/src/index.ts
@@ -3,7 +3,6 @@ import {PLATFORM} from 'aurelia-pal';
 
 export function configure(config: FrameworkConfiguration) {
   config.globalResources([
-    PLATFORM.moduleName('./elements/hello-world'),
     PLATFORM.moduleName('./elements/authService')
   ]);
 }
