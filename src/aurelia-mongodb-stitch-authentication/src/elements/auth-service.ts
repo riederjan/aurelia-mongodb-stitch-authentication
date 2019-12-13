e2d1e4d58ed8c6f71a148bd4d9093c45a3890682
janrieder <jan.rieder@hevs.ch>
2019-12-13 09:09:47 +0100

    Fixed some errors between the files
    Fixed some errors between the files

--- src/aurelia-mongodb-stitch-authentication/src/elements/auth-service.ts
+++ src/aurelia-mongodb-stitch-authentication/src/elements/auth-service.ts
@@ -1,19 +1,19 @@
 import { Stitch, RemoteMongoClient, UserPasswordAuthProviderClient, UserPasswordCredential } from 'mongodb-stitch-browser-sdk';
 
 export class AuthService {
-	appid: string;
-	email: string;
-	password: string;
+	applicationID: string;
+	authEmail: string;
+	authPassword: string;
 
 	show(what) {
 		if (what == "appid") {
-			console.log(this.appid);
+			console.log(this.applicationID);
 		}
 		else if (what == "email") {
-			console.log(this.email);
+			console.log(this.authEmail);
 		}
 		else if (what == "password") {
-			console.log(this.password);
+			console.log(this.authPassword);
 		}
 	}
 
