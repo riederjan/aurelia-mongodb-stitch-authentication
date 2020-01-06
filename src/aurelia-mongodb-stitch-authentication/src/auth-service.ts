dafbe1fafa75894092efbdba81d23c63fdb32381
janrieder <jan.rieder@hevs.ch>
2020-01-06 16:43:13 +0100

    Removed "show" class and improved the login class with a return for the app

--- src/aurelia-mongodb-stitch-authentication/src/auth-service.ts
+++ src/aurelia-mongodb-stitch-authentication/src/auth-service.ts
@@ -7,46 +7,22 @@ export class AuthService {
 	authenticated: boolean = false;
 	client: string;
 
-	show(what) {
-		if (what == "appid") {
-			console.log("appid: " + this.applicationID);
-		}
-		else if (what == "email") {
-			console.log("email: " + this.authEmail);
-		}
-		else if (what == "password") {
-			console.log("password: " + this.authPassword);
-		}
-		else if (what == "authstate") {
-			console.log("authstate: " + this.authenticated);
-		}
-	}
+	login(authEmail?: string, authPassword?: string, applicationID?: string): Promise<any> {
+		let client = Stitch.initializeAppClient(applicationID);
+		let format;
+		if (typeof authEmail === 'object') {
 
-	login() {
-		let client = Stitch.initializeAppClient(this.applicationID);
-		client.auth
-			.loginWithCredential(new UserPasswordCredential(this.authEmail, this.authPassword))
-			.then(authedUser => {
-				console.log("You logged in successfully!");
-				this.authenticated = true;
-			})
-			.catch(error => {
-				// Errorcode 46 on wrong input.
-				if (error.errorCode == "46") {
-					console.log("Invalid username or password.");
-				}
-				// Errorcode 5 on no input.
-				else if (error.errorCode == "5") {
-					console.log("Please enter valid informations.");
-				}
-			});
+		}
+		else if (typeof authEmail === 'string') {
+			format.authEmail = {
+				'email': authEmail,
+				'password': authPassword
+			};
+		}
+		return client.auth.loginWithCredential(new UserPasswordCredential(authEmail, authPassword));
 	}
 
 	logout() {
-		
-	}
 
-	updateAuthenticated() {
-		let wasAuthenticated = this.authenticated;
 	}
 }
