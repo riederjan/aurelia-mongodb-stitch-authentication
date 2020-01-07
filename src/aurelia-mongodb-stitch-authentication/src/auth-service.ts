1c7fe3c6a8aa904bec446c8964d5cc897e0039f3
janrieder <jan.rieder@hevs.ch>
2020-01-07 16:18:41 +0100

    Took the "client" variable out of the class

--- src/aurelia-mongodb-stitch-authentication/src/auth-service.ts
+++ src/aurelia-mongodb-stitch-authentication/src/auth-service.ts
@@ -1,15 +1,15 @@
 import { Stitch, RemoteMongoClient, UserPasswordAuthProviderClient, UserPasswordCredential } from 'mongodb-stitch-browser-sdk';
 import { applicationID } from './appID.json';
 
+let client = Stitch.initializeAppClient(applicationID);
+
 export class AuthService {
 	authEmail: string;
 	authPassword: string;
-	authenticated: boolean = false;
-	client: string;
+	authenticated: boolean;
 
 	login(authEmail?: string, authPassword?: string): Promise<any> {
 		console.log(applicationID);
-		let client = Stitch.initializeAppClient(applicationID);
 		return client.auth.loginWithCredential(new UserPasswordCredential(authEmail, authPassword));
 	}
 
