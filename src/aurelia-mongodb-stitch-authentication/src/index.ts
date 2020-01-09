b23ff22038a307df352232a18553b6f9c62ee2f6
janrieder <jan.rieder@hevs.ch>
2020-01-09 16:26:52 +0100

    Removed unused import

--- src/aurelia-mongodb-stitch-authentication/src/index.ts
+++ src/aurelia-mongodb-stitch-authentication/src/index.ts
@@ -1,7 +1,6 @@
-import {FrameworkConfiguration} from 'aurelia-framework';
-import {Container} from "aurelia-dependency-injection";
-import {PLATFORM} from 'aurelia-pal';
-import { AuthService } from './auth-service';
+import { FrameworkConfiguration } from 'aurelia-framework';
+import { Container } from "aurelia-dependency-injection";
+import { AuthService } from "./auth-service";
 
 export function configure(config: FrameworkConfiguration) {
     config.globalResources([
