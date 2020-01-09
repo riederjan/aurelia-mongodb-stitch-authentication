a0d9af11a0eda9b27322c6594b4f846af3bef446
janrieder <jan.rieder@hevs.ch>
2020-01-09 13:33:56 +0100

    Only create one insctance of AuthService while the app is used

--- src/aurelia-mongodb-stitch-authentication/src/index.ts
+++ src/aurelia-mongodb-stitch-authentication/src/index.ts
@@ -1,10 +1,17 @@
 import {FrameworkConfiguration} from 'aurelia-framework';
+import {Container} from "aurelia-dependency-injection";
 import {PLATFORM} from 'aurelia-pal';
+import { AuthService } from './auth-service';
 
 export function configure(config: FrameworkConfiguration) {
-  config.globalResources([
-	//
+    config.globalResources([
+
 ]);
+  let container: Container = config.container;
+  let instance = new AuthService();
+
+  container.registerInstance(AuthService, instance);
+
 }
 
 export { AuthService } from './auth-service'
