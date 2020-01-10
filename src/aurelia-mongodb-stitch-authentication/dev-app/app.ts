ad0d77f2fe7bfbb944b647163d231d6b5738c807
janrieder <jan.rieder@hevs.ch>
2020-01-10 14:21:07 +0100

    Added a debug section and fixed a bug in app.ts

--- src/aurelia-mongodb-stitch-authentication/dev-app/app.ts
+++ src/aurelia-mongodb-stitch-authentication/dev-app/app.ts
@@ -1,25 +1,20 @@
 import { RouterConfiguration, Router } from 'aurelia-router';
-import { autoinject } from 'aurelia-framework';
 import { PLATFORM } from 'aurelia-framework';
 import { AuthService } from 'resources';
 import 'style.css';
 
-@autoinject
 export class App {
 	authService: AuthService;
 	authEmail: string;
 	authPassword: string;
-
 	router: Router;
 
 	configureRouter(config: RouterConfiguration, router: Router): void {
-		this.router = router;
     config.title = "Aurelia MongoDB Stitch Authentication";
     config.map([
       {route: 'login', name: 'login', title: 'Login', nav: true, moduleId: PLATFORM.moduleName('login')},
-			{route: ['', 'general'], name: 'general', title: 'General', nav: true, moduleId: PLATFORM.moduleName('general')}
+			{route: ['', 'general'], name: 'general', title: 'General', nav: true, moduleId: PLATFORM.moduleName('general')},
     ]);
-
-    this.router = router;
+		this.router = router;
   }
 }
