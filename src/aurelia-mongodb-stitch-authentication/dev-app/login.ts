9cd4f5d41382aaa7d9586ab0b3b4bd860d09ac99
janrieder <jan.rieder@hevs.ch>
2020-01-13 13:20:08 +0100

    Replaced all console.log with an output on the website.

--- src/aurelia-mongodb-stitch-authentication/dev-app/login.ts
+++ src/aurelia-mongodb-stitch-authentication/dev-app/login.ts
@@ -8,6 +8,8 @@ export class Login {
 	authService: AuthService;
 	authEmail: string;
 	authPassword: string;
+	consoleOutput0 : any;
+	consoleError: any;
 
 	constructor(authService: AuthService){
 		const authConfig = {
@@ -20,20 +22,12 @@ export class Login {
 	authenticate(){
 		this.authService.login(this.authEmail, this.authPassword)
 			.then(() => {
-				console.log("Login successfully");
 				location.reload();
+				this.consoleOutput0 = "Successfully logged in";
 			})
 			.catch(error => {
-				// Errorcode 46 on wrong input.
-				if (error.errorCode == "46") {
-					console.log("Invalid username or password.");
-					console.log(error);
-				}
-				// Errorcode 5 on no input.
-				else if (error.errorCode == "5") {
-					console.log("Please enter valid informations.");
-					console.log(error);
-				}
+				this.consoleOutput0 = "Login failed with error:";
+				this.consoleError = error + ": ErrorCode: " + error.errorCode;
 			})
 	}
 
@@ -41,16 +35,17 @@ export class Login {
 		if (this.authService.isAuthenticated() == true){
 			this.authService.logout()
 				.then (() => {
-					console.log("Logged Out");
 					location.reload();
 				})
 		}
 		else if (this.authService.isAuthenticated() == false) {
-			console.log("You are not authenticated yet!");
+			this.consoleOutput0 = "You are not authenticated yet!";
+			this.consoleError = null;
 		}
 	}
 
 	isAuthenticated(){
-		console.log(this.authService.isAuthenticated());
+		this.consoleOutput0 = "Authentication State: " + this.authService.isAuthenticated();
+		this.consoleError = null;
 	}
 }
